536b9d8e9e289eb76cc5bc09a63803ca33e9aa8b
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2025-03-05 17:25:42 +0530

    Added pet system, functional friend system and gamemode selector
    TODO:
    add party system

--- src/LobbyCore/main.php
+++ src/LobbyCore/main.php
@@ -3,20 +3,40 @@
 declare(strict_types=1);
 
 namespace LobbyCore;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\player\Player;
 use pocketmine\utils\Config;
+use jojoe77777\FormAPI\CustomForm;
 use jojoe77777\FormAPI\SimpleForm;
 use ItzLightyHD\KnockbackFFA\event\PlayerKillEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\VanillaItems;
+use pocketmine\item\Item;
+use pocketmine\entity\EntityFactory;
+use pocketmine\entity\Location;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
+use pocketmine\entity\animal\walking\Wolf;
+use pocketmine\entity\animal\walking\Cat;
+use pocketmine\entity\animal\flying\Parrot;
+use pocketmine\entity\Living;
+use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat;
+
 
 class Main extends PluginBase implements Listener {
 
     private Config $friends;
     private Config $friendRequests;
     private Config $levels;
+    private array $pets = [];
+    private array $petNames = [];
 
     public function onEnable(): void {
         $this->getLogger()->info("LobbyCore has been enabled!");
@@ -28,6 +48,69 @@ class Main extends PluginBase implements Listener {
         $this->friendRequests = new Config($this->getDataFolder() . "friendRequests.yml", Config::YAML);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    private function giveLobbyItems(Player $player): void {
+        $inventory = $player->getInventory();
+        $inventory->clearAll();
+        
+        $gameSelector = VanillaItems::COMPASS()->setCustomName("§aGame Selector");
+        $cosmeticsMenu = VanillaItems::TOTEM()->setCustomName("§bCosmetics");
+        $friendsMenu = VanillaItems::BOOK()->setCustomName("§eFriends");
+        $partyMenu = VanillaItems::CLOCK()->setCustomName("§6Party");
+
+        $inventory->setItem(0, $gameSelector);
+        $inventory->setItem(7, $cosmeticsMenu);
+        $inventory->setItem(8, $friendsMenu);
+        $inventory->setItem(9, $partyMenu);
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+
+        switch ($item->getCustomName()) {
+            case "§aGame Selector":
+                $this->openGameSelector($player);
+                break;
+            case "§bCosmetics":
+                $this->openCosmeticsMenu($player);
+                break;
+            case "§eFriends":
+                $this->openFriendMenu($player);
+                break;
+            case "§6Party":
+                $player->sendMessage("§cParty menu is not functional yet!");
+                break;
+        }
+    }
+
+    private function openCosmeticsMenu(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data) {
+            if ($data === null) return;
+            
+            if ($data === 0) {
+                $this->getServer()->dispatchCommand($player, "pets");
+            }
+        });
+        
+        $form->setTitle("Cosmetics Menu");
+        $form->addButton("§bOpen Pets Menu");
+        
+        $player->sendForm($form);
+    }
+    private function openGameSelector(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data) {
+            if ($data === null) return;
+            if ($data === 0) {
+                $player->teleport(new Vector3(100, 50, 100)); // Change to KBFFA spawn coordinates
+                $player->sendMessage("§aTeleporting to Knockback FFA!");
+            }
+        });
+
+        $form->setTitle("Game Selector");
+        $form->addButton("Knockback FFA");
+        $player->sendForm($form);
     }
 
     public function getLevel(Player $player): int {
@@ -62,7 +145,19 @@ class Main extends PluginBase implements Listener {
         return 30;
     }
 
-    public function openFriendMenu(Player $player): void {
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return true;
+        }
+
+        if ($command->getName() === "friend") {
+            $this->openFriendMenu($sender);
+        }
+        return true;
+    }
+
+    private function openFriendMenu(Player $player): void {
         $form = new SimpleForm(function (Player $player, ?int $data) {
             if ($data === null) return;
             
@@ -73,31 +168,28 @@ class Main extends PluginBase implements Listener {
                 case 1:
                     $this->viewFriendRequests($player);
                     break;
+                case 2:
+                    $this->viewFriendsList($player);
+                    break;
             }
         });
 
         $form->setTitle("Friends Menu");
-        $form->setContent("Select an option:");
         $form->addButton("Add Friend");
         $form->addButton("View Requests");
+        $form->addButton("View Friends List");
         
         $player->sendForm($form);
     }
 
     private function sendFriendRequestForm(Player $player): void {
-        $form = new SimpleForm(function (Player $player, ?int $data) {
-            if ($data === null) return;
-            
-            $targetName = array_values($this->getServer()->getOnlinePlayers())[$data]->getName();
-            $this->sendFriendRequest($player, $targetName);
+        $form = new CustomForm(function (Player $player, ?array $data) {
+            if ($data === null || empty($data[0])) return;
+            $this->sendFriendRequest($player, $data[0]);
         });
         
-        $form->setTitle("Select a Player");
-        foreach ($this->getServer()->getOnlinePlayers() as $onlinePlayer) {
-            if ($onlinePlayer !== $player) {
-                $form->addButton($onlinePlayer->getName());
-            }
-        }
+        $form->setTitle("Add Friend");
+        $form->addInput("Enter the player's username:");
         
         $player->sendForm($form);
     }
@@ -119,10 +211,9 @@ class Main extends PluginBase implements Listener {
         $requests = $this->friendRequests->get($name, []);
         
         $form = new SimpleForm(function (Player $player, ?int $data) use ($requests) {
-            if ($data === null) return;
+            if ($data === null || !isset($requests[$data])) return;
             
-            $targetName = $requests[$data];
-            $this->acceptFriendRequest($player, $targetName);
+            $this->handleFriendRequestResponse($player, $requests[$data]);
         });
         
         $form->setTitle("Friend Requests");
@@ -134,6 +225,24 @@ class Main extends PluginBase implements Listener {
             }
         }
         
+        $player->sendForm($form);
+    }
+
+    private function handleFriendRequestResponse(Player $player, string $targetName): void {
+        $form = new SimpleForm(function (Player $player, ?int $data) use ($targetName) {
+            if ($data === null) return;
+            
+            if ($data === 0) {
+                $this->acceptFriendRequest($player, $targetName);
+            } else {
+                $this->declineFriendRequest($player, $targetName);
+            }
+        });
+
+        $form->setTitle("Friend Request from {$targetName}");
+        $form->addButton("Accept");
+        $form->addButton("Decline");
+
         $player->sendForm($form);
     }
 
@@ -150,5 +259,31 @@ class Main extends PluginBase implements Listener {
         $this->friendRequests->save();
 
         $player->sendMessage("§aYou are now friends with {$targetName}!");
+    }
+
+    private function declineFriendRequest(Player $player, string $targetName): void {
+        $name = $player->getName();
+        $requests = $this->friendRequests->get($name, []);
+        $requests = array_diff($requests, [$targetName]);
+        $this->friendRequests->set($name, $requests);
+        $this->friendRequests->save();
+
+        $player->sendMessage("§cYou declined the friend request from {$targetName}.");
+    }
+
+    private function viewFriendsList(Player $player): void {
+        $name = $player->getName();
+        $friends = $this->friends->get($name, []);
+        
+        $form = new SimpleForm(function (Player $player, ?int $data) {});
+        $form->setTitle("Your Friends");
+        if (empty($friends)) {
+            $form->setContent("You have no friends added.");
+        } else {
+            foreach ($friends as $friend) {
+                $form->addButton($friend);
+            }
+        }
+        $player->sendForm($form);
     }
 }
